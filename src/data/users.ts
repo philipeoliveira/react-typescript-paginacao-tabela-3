fcbabd3f02a8a1e308fec5bbefb3c3ff97f34dfd
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-11-20 19:10:22 -0300

    Create user data with fakerjs

---
+++ src/data/users.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker';
+
+export const users = Array.from({ length: 215 }).map(() => {
+   return {
+      id: faker.number.int({ min: 10000, max: 99999 }),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+   };
+});
