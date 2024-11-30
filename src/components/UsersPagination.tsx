f7930d753f55d39ae629d96d863aea9376a2e323
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-11-30 10:43:30 -0300

    Creates user pagination content

---
+++ src/components/UsersPagination.tsx
@@ -0,0 +1,78 @@
+import data from '../data/users';
+
+import { Table } from './table/Table';
+import { TableHeader } from './table/TableHeader';
+import { TableCell } from './table/TableCell';
+import { TableRow } from './table/TableRow';
+
+import { usePagination } from '../hooks/usePagination';
+import { Pagination } from './Pagination';
+
+export function UsersPagination() {
+   const {
+      currentPage,
+      limitPerPage,
+      totalPages,
+      totalButtons,
+      handleItemsPerPage,
+      firstButton,
+      handlePagination,
+   } = usePagination(data);
+
+   return (
+      <>
+         <Table>
+            <thead>
+               <TableRow>
+                  <TableHeader>Código</TableHeader>
+                  <TableHeader>Nome</TableHeader>
+                  <TableHeader>Sobrenome</TableHeader>
+                  <TableHeader otherClasses='max-sm:hidden'>E-mail</TableHeader>
+               </TableRow>
+            </thead>
+            <tbody>
+               {data
+                  .slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage)
+                  .map((user) => {
+                     return (
+                        <TableRow key={user.id}>
+                           <TableCell>{user.id}</TableCell>
+                           <TableCell>{user.firstName}</TableCell>
+                           <TableCell>{user.lastName}</TableCell>
+                           <TableCell otherClasses='max-sm:hidden'>
+                              {user.email}
+                           </TableCell>
+                        </TableRow>
+                     );
+                  })}
+            </tbody>
+            <tfoot>
+               <tr>
+                  <TableCell colSpan={2} otherClasses='text-sm'>
+                     {limitPerPage} de {data.length} itens
+                  </TableCell>
+
+                  <TableCell colSpan={2} otherClasses='text-sm text-right'>
+                     <div className='flex max-sm:flex-col items-center justify-end gap-4 py-3'>
+                        <span>
+                           Página {currentPage} de {totalPages}
+                        </span>
+                     </div>
+                  </TableCell>
+               </tr>
+            </tfoot>
+         </Table>
+         <Pagination
+            pagination={{
+               currentPage,
+               limitPerPage,
+               totalPages,
+               totalButtons,
+               handleItemsPerPage,
+               firstButton,
+               handlePagination,
+            }}
+         />
+      </>
+   );
+}
